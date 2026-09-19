65b93659277697cd10bf98cf1977053172780d30
kierandrewett <kieran@drewett.dev>
2026-09-19 18:36:44 +0100

    test: link TOML parser in configuration regression suite

--- tests/test-config.sh
+++ tests/test-config.sh
@@ -11,6 +11,8 @@ sources=(
     "$ROOT/src/config/gnoblin-config.c"
     "$ROOT/src/config/gnoblin-lua.c"
     "$ROOT/src/config/gnoblin-glob.c"
+    "$ROOT/src/config/gnoblin-toml.c"
+    "$ROOT/src/config/tomlc99/toml.c"
 )
 for test in lua-config glob-config lua-console; do
     cc "$ROOT/tests/$test-test.c" "${sources[@]}" \
